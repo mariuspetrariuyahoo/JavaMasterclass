934a894a55415eec611893af09e64c6ab1411ffa
Marius <mariuspetrariu@gmail.com>
2025-04-03 07:58:00 +0300

    First Commit

--- src/DiagonalStar.java
+++ src/DiagonalStar.java
@@ -1,26 +1,34 @@
 public class DiagonalStar {
     public static void printSquareStar(int number) {
         if (number < 5) {
-            System.out.println("Invalid Number");
+            System.out.println("Invalid Value");
             return;
         }
         for (int row = 1; row <= number; row++) {
-            for (int column = 1; column <= number; column++) {
-
-                if (row == 1 || row == column) {
+            for (int col = 1; col <= number; col++) {
+                if (row == 1 || col == 1) {
                     System.out.print("*");
-                    break;
+                    continue;
                 }
-                ;
-                if ((column == 1 || column == number)) {
+                if (row == number || col == number) {
                     System.out.print("*");
-                    break;
+                    continue;
                 }
-                if ((row == column)) {
+                if (row == col) {
                     System.out.print("*");
-
+                    continue;
                 }
+                if (col == number - row + 1) {
+                    System.out.print("*");
+                    continue;
+                }
+              System.out.print(" ");
             }
+            System.out.println();
         }
     }
+
+//    public static void main(String[] args) {
+//        printSquareStar(9);
+//    }
 }
